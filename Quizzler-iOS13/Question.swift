ae678a814ef1381248c2eee47099efe8c55bd656
Victor Deon <victorhad@gmail.com>
2025-05-06 16:59:21 -0300

    Criando a logica dentro da tela

---
+++ Quizzler-iOS13/Question.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+struct Question {
+    let title: String
+    let answer: Bool
+    
+    init(title: String, answer: Bool) {
+        self.title = title
+        self.answer = answer
+    }
+}
